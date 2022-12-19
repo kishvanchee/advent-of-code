67d1d286fc4e0ae158bf6faf1eb3b55db9a9a61a
Kishore Vancheeshwaran <kvancheeshwa@paloaltonetworks.com>
2022-12-19 20:03:16 +0530

    day 3 python p2

--- 2022/python/day03.py
+++ 2022/python/day03.py
@@ -2,18 +2,32 @@
 
 import sys
 from utils import timing
+import string
 
 filename = sys.argv[1]
 with open(filename, 'r') as fin:
-    lines = fin.readlines()
+    lines = [line.strip() for line in fin.readlines()]
+
+def get_priority(letter):
+    return string.ascii_letters.index(letter) + 1
 
 
 @timing
 def main():
     part1_score = 0
+    part2_score = 0
 
-    first = [None]*52
-    second = [None]*52
+    groups = list(zip(*(iter(lines),) * 3))
 
-    for line in lines:
-        pass
+    for a,b,c in groups:
+        part2_score += get_priority(
+            list(
+                set(a).intersection(set(b)).intersection(set(c))
+            )[0]
+        )
+
+    print('part2 score', part2_score)
+
+
+if __name__ == '__main__':
+    main()
